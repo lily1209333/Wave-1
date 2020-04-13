eb098004cc1406658a774bd64e68636803ff6f97
Lily <53480409+lily1209333@users.noreply.github.com>
2020-04-13 15:54:07 -0400

    Add files via upload

---
+++ interest.py
@@ -0,0 +1,8 @@
+initial = input("deposit amount of money here: $")
+ini = float(initial)
+first = ini * 1.04
+sec = first * 1.04
+third = sec * 1.04
+print("First year: $", round(first, 1))
+print("Second year: $", round(sec, 1))
+print("Third year: $", round(third, 1))
